c1a62e1c926ad7b2cf55f34837bfeea226514de1
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-11-12 20:16:09 +0100

    Add exercises

---
+++ Basics/src/FirstStepsInCoding/P02/P01_USD_to_BGN.java
@@ -0,0 +1,14 @@
+package FirstStepsInCoding.P02;
+
+import java.util.Scanner;
+
+public class P01_USD_to_BGN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double usdMoney=Double.parseDouble(scanner.nextLine());
+        double conversionRate=1.79549;
+        double  bgnMoney=usdMoney*conversionRate;
+        System.out.println(bgnMoney);
+
+    }
+}
---
+++ Basics/src/FirstStepsInCoding/P02/P02_Radians_to_Degrees.java
@@ -0,0 +1,10 @@
+package FirstStepsInCoding.P02;
+
+import java.util.Scanner;
+
+public class P02_Radians_to_Degrees {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
