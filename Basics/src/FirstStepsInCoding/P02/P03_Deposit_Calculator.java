23c4375b3d92f84fb12e2c19aa4b0e4c6a544353
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-11-13 21:36:35 +0100

    Add excercises

---
+++ Basics/src/FirstStepsInCoding/P02/P03_Deposit_Calculator.java
@@ -0,0 +1,18 @@
+package FirstStepsInCoding.P02;
+
+import java.util.Scanner;
+
+public class P03_Deposit_Calculator {
+    public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+
+   double deposit=Double.parseDouble(scanner.nextLine());
+   int depositTimeFrame =Integer.parseInt(scanner.nextLine());
+   double yearlyRate=Double.parseDouble(scanner.nextLine())/100;
+
+        double sum = deposit + depositTimeFrame*((deposit*yearlyRate)/12);
+        System.out.println(sum);
+
+
+    }
+}
