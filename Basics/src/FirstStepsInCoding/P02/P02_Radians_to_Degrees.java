15a05dc336e6fb533d2cc1d90a3dea816f1d0a62
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-11-13 20:44:04 +0100

    Add exercises

--- Basics/src/FirstStepsInCoding/P02/P02_Radians_to_Degrees.java
+++ Basics/src/FirstStepsInCoding/P02/P02_Radians_to_Degrees.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class P02_Radians_to_Degrees {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        double radians= Double.parseDouble(scanner.nextLine());
+        double radius= radians*180 / Math.PI;
+        System.out.println(radius);
     }
 }
