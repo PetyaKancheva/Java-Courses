8fd1ba1cc4a3604cb697b3f59eba1b2faa16cb8b
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2024-11-18 20:13:00 +0100

    Add exercises

---
+++ Basics/src/FirstStepsInCoding/P02/P05SuppliesforSchool.java
@@ -0,0 +1,10 @@
+package FirstStepsInCoding.P02;
+
+import java.util.Scanner;
+
+public class P05SuppliesforSchool {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
