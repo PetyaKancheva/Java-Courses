e3ec56e683e7bf68241e984667929a6959b2ad74
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-02-04 06:13:55 +0100

    Add exercises

--- Basics/src/ConditionalStatementsAdvanced/P02/P06OperationsBetweenNumbers.java
+++ Basics/src/ConditionalStatementsAdvanced/P02/P06OperationsBetweenNumbers.java
@@ -8,13 +8,43 @@ public class P06OperationsBetweenNumbers {
          int firstNumber=Integer.parseInt((scanner.nextLine()));
          int secondNumber=Integer.parseInt((scanner.nextLine()));
          String operator=scanner.nextLine();
-// only of division
-         if (firstNumber==0 ){
-             System.out.printf("Cannot divide %d by zero",secondNumber);
-         }else if(secondNumber==0){
-             System.out.printf("Cannot divide %d by zero",firstNumber);
-         }else{
 
-         }
+         double result=0;
+
+        if (operator.equals("+")){
+            result=firstNumber+secondNumber;
+            System.out.printf("%d %s %d = %.0f - %s",firstNumber,operator,secondNumber,result,isEven(result));
+        }else if (operator.equals("-")){
+            result=firstNumber-secondNumber;
+            System.out.printf("%d %s %d = %.0f - %s",firstNumber,operator,secondNumber,result,isEven(result));
+        }else if (operator.equals("*")){
+            result=firstNumber*secondNumber;
+            System.out.printf("%d %s %d = %.0f - %s",firstNumber,operator,secondNumber,result,isEven(result));
+        }else if (operator.equals("/")){
+            if (firstNumber==0 ){
+                System.out.printf("Cannot divide %d by zero",secondNumber);
+            }else if(secondNumber==0){
+                System.out.printf("Cannot divide %d by zero",firstNumber);
+            }else{
+                result=firstNumber*1.0/secondNumber;
+                System.out.printf("%d %s %d = %.2f",firstNumber,operator,secondNumber,result);
+            }
+        }else if(operator.equals("%")){
+            if (firstNumber==0 ){
+                System.out.printf("Cannot divide %d by zero",secondNumber);
+            }else if(secondNumber==0){
+                System.out.printf("Cannot divide %d by zero",firstNumber);
+            }else{
+                result=firstNumber%secondNumber;
+                System.out.printf("%d %s %d = %.0f",firstNumber,operator,secondNumber,result);
+            }
+        }
+
+    }
+    public static String isEven(double num){
+        if(num%2==0){
+            return "even";
+        }
+        return "odd";
     }
 }
---
+++ Basics/src/ConditionalStatementsAdvanced/P02/P07HotelRoom.java
@@ -0,0 +1,9 @@
+package ConditionalStatementsAdvanced.P02;
+
+import java.util.Scanner;
+
+public class P07HotelRoom {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
