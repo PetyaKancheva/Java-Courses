6a270eb80d3cfb29ab0ab20ac25859ea65fce430
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-02-10 21:15:07 +0100

    Add exercises

--- Basics/src/WhileLoop/P02/P05Coins.java
+++ Basics/src/WhileLoop/P02/P05Coins.java
@@ -1,10 +1,44 @@
 package WhileLoop.P02;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class P05Coins {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+         BigDecimal inputMoney= new BigDecimal(scanner.nextLine());
+         int coinsCount=0;
+         while(inputMoney.compareTo(BigDecimal.ZERO)>0){
+
+             if(inputMoney.compareTo(BigDecimal.valueOf(2))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(2));
+                 coinsCount++;
+             }   else if(inputMoney.compareTo(BigDecimal.valueOf(1))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(1));
+                 coinsCount++;
+             }  else if(inputMoney.compareTo(BigDecimal.valueOf(0.5))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(0.5));
+                 coinsCount++;
+             }  else if(inputMoney.compareTo(BigDecimal.valueOf(0.2))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(0.2));
+                 coinsCount++;
+             }  else if(inputMoney.compareTo(BigDecimal.valueOf(0.1))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(0.1));
+                 coinsCount++;
+             }  else if(inputMoney.compareTo(BigDecimal.valueOf(0.05))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(0.05));
+                 coinsCount++;
+             }  else if(inputMoney.compareTo(BigDecimal.valueOf(0.02))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(0.02));
+                 coinsCount++;
+             }  else if(inputMoney.compareTo(BigDecimal.valueOf(0.01))>=0) {
+                 inputMoney=   inputMoney.subtract(BigDecimal.valueOf(0.01));
+                 coinsCount++;
+             }
+
+         }
+        System.out.println(coinsCount);
 
     }
 }
