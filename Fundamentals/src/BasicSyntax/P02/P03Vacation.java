b73d6dace66423a9973682424a38462c039f9a52
PetyaKancheva <105904595+PetyaKancheva@users.noreply.github.com>
2025-06-01 13:15:33 +0300

    Add exercises

---
+++ Fundamentals/src/BasicSyntax/P02/P03Vacation.java
@@ -0,0 +1,14 @@
+package BasicSyntax.P02;
+
+import java.util.Scanner;
+
+public class P03Vacation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int groupNumber=Integer.parseInt(scanner.nextLine());
+        String groupType=scanner.nextLine();
+        String dayOfWeek=scanner.nextLine();
+        
+
+    }
+}
